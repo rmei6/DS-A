4a7f2095471162088f056d91150203d7b5e9f5a6
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2023-05-08 15:59:15 -0400

    weekly hackerrank

---
+++ Binary_Search.py
@@ -0,0 +1,13 @@
+def search(nums, target):
+    # Write your code here
+    left = 0
+    right = len(nums)-1    
+    while left<=right:
+        mid = (left+right)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]>target:
+            right = mid-1
+        else:
+            left = mid+1
+    return -1
---
+++ Fizz_Buzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    # Write your code here
+    answer = []
+    for i in range(1,n+1):
+        if(i % 3 == 0 and i % 5 == 0):
+            answer.append("FizzBuzz")
+        elif(i % 3 == 0):
+            answer.append("Fizz")
+        elif(i % 5 == 0):
+            answer.append("Buzz")
+        else:
+            answer.append(str(i))
+    return answer

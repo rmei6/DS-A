ffb16a5b39556dfd11c36b5e03699e3872b34003
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2023-06-30 21:12:34 -0400

    matrix problems

---
+++ Rotate_List.py
@@ -0,0 +1,58 @@
+# Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [4,5,1,2,3]
+# Example 2:
+
+
+# Input: head = [0,1,2], k = 4
+# Output: [2,0,1]
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 500].
+# -100 <= Node.val <= 100
+# 0 <= k <= 2 * 109
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        lastElement = head
+        length = 1
+        # get the length of the list and the last node in the list
+        while ( lastElement.next ):
+            lastElement = lastElement.next
+            length += 1
+
+        # If k is equal to the length of the list then k == 0
+        # ElIf k is greater than the length of the list then k = k % length
+        k = k % length
+            
+        # Set the last node to point to head node
+        # The list is now a circular linked list with last node pointing to first node
+        lastElement.next = head
+        
+        # Traverse the list to get to the node just before the ( length - k )th node.
+        tempNode = head
+        for _ in range( length - k - 1 ):
+            tempNode = tempNode.next
+        
+        # Get the next node from the tempNode and then set the tempNode.next as None
+        answer = tempNode.next
+        tempNode.next = None
+        
+        return answer

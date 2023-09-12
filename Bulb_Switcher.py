cb43cbdc30d0350d8b2c270055be34dd043e36dd
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2023-09-12 14:15:06 -0400

    hackerrank

---
+++ Bulb_Switcher.py
@@ -0,0 +1,45 @@
+# There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+
+# On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+
+# Return the number of bulbs that are on after n rounds.
+
+ 
+
+# Example 1:
+
+
+# Input: n = 3
+# Output: 1
+# Explanation: At first, the three bulbs are [off, off, off].
+# After the first round, the three bulbs are [on, on, on].
+# After the second round, the three bulbs are [on, off, on].
+# After the third round, the three bulbs are [on, off, off]. 
+# So you should return 1 because there is only one bulb is on.
+# Example 2:
+
+# Input: n = 0
+# Output: 0
+# Example 3:
+
+# Input: n = 1
+# Output: 1
+ 
+
+# Constraints:
+
+# 0 <= n <= 109
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(n**(1/2))
+    #math solution
+    #only have as many bulbs on as square root of turn
+    # Round 2 : 1 0
+    # Round 3 : 1 0 0
+    # Round 4 : 1 0 0 1
+    # Round 5 : 1 0 0 1 0
+    # Round 6 : 1 0 0 1 0 0
+    # Round 7 : 1 0 0 1 0 0 0
+    # Round 8 : 1 0 0 1 0 0 0 0
+    # Round 9 : 1 0 0 1 0 0 0 0 1

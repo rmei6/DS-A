9885477d625467c3762c1b669e877f4b1ffe6e97
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2023-09-20 22:16:09 -0400

    tedious

---
+++ Sudoku_Solver.py
@@ -0,0 +1,75 @@
+# Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+# A sudoku solution must satisfy all of the following rules:
+
+# Each of the digits 1-9 must occur exactly once in each row.
+# Each of the digits 1-9 must occur exactly once in each column.
+# Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+# The '.' character indicates empty cells.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+# Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit or '.'.
+# It is guaranteed that the input board has only one solution.
+
+from typing import List
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = 9
+        
+        
+        def isValid(row, col, ch):
+            row, col = int(row), int(col)
+            
+            for i in range(9):
+                
+                if board[i][col] == ch:
+                    return False
+                if board[row][i] == ch:
+                    return False
+                
+                if board[3*(row//3) + i//3][3*(col//3) + i%3] == ch:
+                    return False
+            
+            return True
+            
+        def solve(row, col):
+            if row == n:
+                return True
+            if col == n:
+                return solve(row+1, 0)
+            
+            if board[row][col] == ".":
+                for i in range(1, 10):
+                    if isValid(row, col, str(i)):
+                        board[row][col] = str(i)
+                        
+                        if solve(row, col + 1):
+                            return True
+                        else:
+                            board[row][col] = "."
+                return False
+            else:
+                return solve(row, col + 1)
+            
+            
+        
+        solve(0, 0)

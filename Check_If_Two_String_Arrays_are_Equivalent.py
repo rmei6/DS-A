c9b13bc98cf090e116820e70feefd649a6f3afa8
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2023-11-30 20:24:00 -0500

    simpler and faster

--- Check_If_Two_String_Arrays_are_Equivalent.py
+++ Check_If_Two_String_Arrays_are_Equivalent.py
@@ -33,13 +33,16 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        for c1, c2 in zip(self.generate(word1), self.generate(word2)):
-            if c1 != c2:
-                return False
-        return True
+        word1 = "".join(word1)
+        word2 = "".join(word2)
+        return word1 == word2
+    #     for c1, c2 in zip(self.generate(word1), self.generate(word2)):
+    #         if c1 != c2:
+    #             return False
+    #     return True
 
-    def generate(self, wordlist: List[str]):
-        for word in wordlist:
-            for char in word:
-                yield char
-        yield None
+    # def generate(self, wordlist: List[str]):
+    #     for word in wordlist:
+    #         for char in word:
+    #             yield char
+    #     yield None

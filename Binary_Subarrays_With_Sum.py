c643a0edf16b182453414610533a44240ca8c02a
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2024-03-13 22:00:05 -0400

    adjustments

--- Binary_Subarrays_With_Sum.py
+++ Binary_Subarrays_With_Sum.py
@@ -29,11 +29,12 @@ from typing import List
 
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        pri={0:1}
-        res=0
-        summ=0
+        # sliding window
+        pri = {0:1}
+        res = 0
+        summ = 0
         for i in nums:
-            summ+=i
-            res+=pri.get(summ-goal,0)
-            pri[summ]=pri.get(summ,0)+1
+            summ += i
+            res += pri.get(summ - goal,0)
+            pri[summ] = pri.get(summ,0) + 1
         return res

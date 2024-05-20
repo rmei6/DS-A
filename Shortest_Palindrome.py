076bcc8466a8edd5cbd88673890c1f52f8e73591
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2024-05-20 15:08:10 -0400

    rolling hash

--- Shortest_Palindrome.py
+++ Shortest_Palindrome.py
@@ -21,9 +21,9 @@
 # 0 <= s.length <= 5 * 104
 # s consists of lowercase English letters only.
 
-# KMP approach 
-# time: O(n), space: O(n) 
 class Solution:
+    # KMP approach 
+    # time: O(n), space: O(n) 
     def shortestPalindrome(self, s: str) -> str:
         r = s[::-1]
         ts = s + "#" + r
@@ -36,3 +36,17 @@ class Solution:
             pi[i] = j
 
         return r[: len(r) - pi[-1]] + s
+    # Rolling Hash approach
+    # time: O(n), space: O(n)
+    def shortestPalindrome1(self, s: str) -> str:
+        n = len(s)
+        P, MOD, POW = 31, 10**9 + 7, 1
+        h1 = h2 = max_pan_pref_len = 0
+        for i in range(n):
+            char_int = ord(s[i]) - ord("a") + 1
+            h1 = (h1 * P + char_int) % MOD
+            h2 = (char_int * POW + h2) % MOD
+            if h1 == h2: max_pan_pref_len = i + 1
+            POW = POW * P % MOD
+
+        return s[max_pan_pref_len:][::-1] + s     

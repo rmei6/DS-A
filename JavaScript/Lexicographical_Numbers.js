b9919418b4a50e4ffab3c26a00cd0045c39c2d26
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2024-09-20 23:14:08 -0400

    lexi

---
+++ JavaScript/Lexicographical_Numbers.js
@@ -0,0 +1,42 @@
+// Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+// You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+
+ 
+
+// Example 1:
+
+// Input: n = 13
+// Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+// Example 2:
+
+// Input: n = 2
+// Output: [1,2]
+ 
+
+// Constraints:
+
+// 1 <= n <= 5 * 104
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function (n) {
+  const arr = [];
+  function dfs(baseIndex) {
+      if (baseIndex * 10 > n) {
+          return;
+      }
+      for (let i = baseIndex * 10; i < baseIndex * 10 + 10 && i <= n; i++) {
+          arr.push(i);
+          dfs(i);
+      }
+  }
+  let stack = [];
+  for (let i = 1; i <= 9 && i <= n; i++) {
+      arr.push(i);
+      dfs(i);
+  }
+  return arr;
+};

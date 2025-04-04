cebff073c67f744f0f3140478dbb0d13264cea27
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2025-04-03 22:22:00 -0400

    dfs with trie

--- JavaScript/Delete_Duplicate_Folders_in_System.js
+++ JavaScript/Delete_Duplicate_Folders_in_System.js
@@ -58,67 +58,139 @@
  * @param {string[][]} paths
  * @return {string[][]}
  */
-class Node {
-  constructor(val, parent){
-      this.val = val;
-      this.parent = parent;
-      this.children = {};
-      this.hash;
-  };
-};
-class Trie {
-  constructor(){
-      this.root = new Node('');
-      this.hashMemo = {};
-  };
-  insert(path){
-      let cur = this.root;
-      for(let node of path)
-          if(cur.children[node] !== undefined)
-              cur = cur.children[node];
-          else
-              cur.children[node] = new Node(node, cur), cur = cur.children[node];
-  };
-  traverse(node = this.root){
-      node.hash = []
-      if(node.children)
-          for(let child of Object.values(node.children))
-              node.hash.push(this.traverse(child))
-      node.hash = node.hash.join('~')
-      if(node.hash !== ''){
-          if(this.hashMemo[node.hash] === undefined)
-              this.hashMemo[node.hash] = [] 
-          this.hashMemo[node.hash].push(node)
-          return node.val + '/[' + node.hash + ']'
-      }
-      return node.val
-  }
-  delete(node = this.root){
-      if(!node || node.hash === undefined)
-          return
-      else if(this.hashMemo[node.hash] && this.hashMemo[node.hash].length > 1)
-          for(let todelete of this.hashMemo[node.hash])
-              delete todelete.parent.children[todelete.val]
-      else
-          for(let child of Object.values(node.children))
-              this.delete(child)
-  }
-  serialize(node = this.root, stack = [], res = []){
-      if(node.val !== '')
-          stack.push(node.val),
-          res.push([...stack])
-      for(let child of Object.values(node.children))
-          this.serialize(child, stack, res)
-      stack.pop()
-      return res
+
+// custom trie approach, passed 106/108 cases
+
+// class Node {
+//   constructor(val, parent){
+//       this.val = val;
+//       this.parent = parent;
+//       this.children = {};
+//       this.hash;
+//   };
+// };
+// class Trie {
+//   constructor(){
+//       this.root = new Node('');
+//       this.hashMemo = {};
+//   };
+//   insert(path){
+//       let cur = this.root;
+//       for(let node of path)
+//           if(cur.children[node] !== undefined)
+//               cur = cur.children[node];
+//           else
+//               cur.children[node] = new Node(node, cur), cur = cur.children[node];
+//   };
+//   traverse(node = this.root){
+//       node.hash = []
+//       if(node.children)
+//           for(let child of Object.values(node.children))
+//               node.hash.push(this.traverse(child))
+//       node.hash = node.hash.join('~')
+//       if(node.hash !== ''){
+//           if(this.hashMemo[node.hash] === undefined)
+//               this.hashMemo[node.hash] = [] 
+//           this.hashMemo[node.hash].push(node)
+//           return node.val + '/[' + node.hash + ']'
+//       }
+//       return node.val
+//   }
+//   delete(node = this.root){
+//       if(!node || node.hash === undefined)
+//           return
+//       else if(this.hashMemo[node.hash] && this.hashMemo[node.hash].length > 1)
+//           for(let todelete of this.hashMemo[node.hash])
+//               delete todelete.parent.children[todelete.val]
+//       else
+//           for(let child of Object.values(node.children))
+//               this.delete(child)
+//   }
+//   serialize(node = this.root, stack = [], res = []){
+//       if(node.val !== '')
+//           stack.push(node.val),
+//           res.push([...stack])
+//       for(let child of Object.values(node.children))
+//           this.serialize(child, stack, res)
+//       stack.pop()
+//       return res
+//   }
+// }
+
+// var deleteDuplicateFolder = function(paths) {
+//   let T = new Trie()
+//   for(let P of paths)
+//       T.insert(P)
+//   T.traverse()
+//   T.delete()
+//   return T.serialize()
+// };
+class TrieNode {
+  constructor() {
+      this.children = new Map()
+      this.del = false
+      this.val = ''
   }
 }
+class Trie {
+  constructor() {
+      this.root = new TrieNode()
+  }
+  insert(path) {
+      let node = this.root
+      for (const folder of path) {
+          if (!node.children.has(folder)) {
+              node.children.set(folder, new TrieNode())
+              node.val = folder
+          }
+          node = node.children.get(folder)
+      }
+  }
+}
+var deleteDuplicateFolder = function (paths) {
+  paths.sort()
+  let trie = new Trie()
+  for (const path of paths) {
+      trie.insert(path)
+  }
+  let root = trie.root
+  let map = new Map()
+  function dfs(node) {
+      if (node.children.size === 0) return node.val
+      let output = ''
+      for (const [k, child] of node.children.entries()) {
+          output += `${k}${dfs(child)}`
+      }
+      if (output) {
+          if (!map.has(output)) {
+              map.set(output, [])
+          }
+          map.get(output).push(node)
+      }
+      return node.val + output
 
-var deleteDuplicateFolder = function(paths) {
-  let T = new Trie()
-  for(let P of paths)
-      T.insert(P)
-  T.traverse()
-  T.delete()
-  return T.serialize()
+  }
+  dfs(root)
+  for (const [k, arr] of map.entries()) {
+      if (arr.length > 1) {
+          for (const node of arr) {
+              node.del = true
+          }
+      }
+  }
+  let output = []
+  function dfs2(node, path) {
+      for (const [k, child] of node.children.entries()) {
+          if (!child.del) {
+
+              dfs2(child, [...path, k])
+          }
+      }
+      if (path.length) {
+          output.push([...path])
+      }
+
+  }
+  dfs2(root, [])
+  return output
 };

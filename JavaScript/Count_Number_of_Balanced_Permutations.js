70678fc9612146b67b95316d0b660d004a1db070
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2025-05-08 23:57:01 -0400

    combinatorics

--- JavaScript/Count_Number_of_Balanced_Permutations.js
+++ JavaScript/Count_Number_of_Balanced_Permutations.js
@@ -45,3 +45,48 @@
 // 2 <= num.length <= 80
 // num consists of digits '0' to '9' only.
 
+/**
+ * @param {string} num
+ * @return {number}
+ */
+var countBalancedPermutations = function (num) {
+  const MOD = 1e9 + 7;
+  const n = num.length;
+  const cnt = Array(10).fill(0);
+  let sum = 0;
+  for (const ch of num) cnt[ch]++, sum += +ch;
+  if (sum % 2) return 0;
+
+  const memo = new Map();
+
+  const comb = (n, r) => {
+    if (r < 0 || r > n) return 0;
+    r = Math.min(r, n - r);
+    let res = 1n;
+    for (let i = 0; i < r; i++) {
+      res = (res * BigInt(n - i)) / BigInt(i + 1);
+    }
+    return Number(res % BigInt(MOD));
+  };
+
+  var dfs = function(i, odd, even, balance) {
+    if (odd === 0 && even === 0 && balance === 0) return 1;
+    if (i < 0 || odd < 0 || even < 0 || balance < 0) return 0;
+    const key = `${i},${odd},${even},${balance}`;
+    if (memo.has(key)) return memo.get(key);
+
+    let res = 0;
+    for (let j = 0; j <= cnt[i]; j++) {
+      const o = j, e = cnt[i] - j;
+      if (odd < o || even < e || balance < i * o) continue;
+      const ways = BigInt(comb(odd, o)) * BigInt(comb(even, e)) % BigInt(MOD);
+      const sub = BigInt(dfs(i - 1, odd - o, even - e, balance - i * o));
+      res = (res + Number(ways * sub % BigInt(MOD))) % MOD;
+    }
+
+    memo.set(key, res);
+    return res;
+  };
+
+  return dfs(9, n - Math.floor(n / 2), Math.floor(n / 2), sum / 2);
+};

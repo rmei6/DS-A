59dbaeb20ceffcf6db4ab0c817cd918aab5e48b5
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2025-06-26 00:10:00 -0400

    learning promise and race

--- JavaScript/Design_Cancellable_Function.js
+++ JavaScript/Design_Cancellable_Function.js
@@ -113,3 +113,43 @@
 
 // cancelledAt == null or 0 <= cancelledAt <= 1000
 // generatorFunction returns a generator object
+
+// promises, async and race
+
+/**
+ * @param {Generator} generator
+ * @return {[Function, Promise]}
+ */
+var cancellable = function(generator) {
+    var cancelled = false;
+    var cancel;
+    const cancelPromise = new Promise((_, reject) => cancel = () => {
+            cancelled = true;
+            reject("Cancelled");
+        });
+    
+    const promise = (async () => {
+        let next = generator.next();
+        while (!cancelled && !next.done) {
+            try {
+                next = generator.next(await Promise.race([next.value, cancelPromise]));
+            } catch (e) {
+                next = generator.throw(e);
+            }
+        }
+
+        return next.value;
+    })();
+    return [cancel, promise];
+};
+
+/**
+ * function* tasks() {
+ *   const val = yield new Promise(resolve => resolve(2 + 2));
+ *   yield new Promise(resolve => setTimeout(resolve, 100));
+ *   return val + 1;
+ * }
+ * const [cancel, promise] = cancellable(tasks());
+ * setTimeout(cancel, 50);
+ * promise.catch(console.log); // logs "Cancelled" at t=50ms
+ */

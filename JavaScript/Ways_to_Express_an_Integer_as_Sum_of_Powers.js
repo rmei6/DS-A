9eec79977ee589176a3b0e0878dc7b270d2fed99
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2025-08-12 00:02:23 -0400

    dynamic programming

--- JavaScript/Ways_to_Express_an_Integer_as_Sum_of_Powers.js
+++ JavaScript/Ways_to_Express_an_Integer_as_Sum_of_Powers.js
@@ -27,3 +27,22 @@
 
 // 1 <= n <= 300
 // 1 <= x <= 5
+
+/**
+ * @param {number} n
+ * @param {number} x
+ * @return {number}
+ */
+var numberOfWays = function(n, x) {
+    const MOD = 1e9 + 7;
+    const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+    for (let a = 1; ; a++) {
+        const power = Math.pow(a, x);
+        if (power > n) break;
+        for (let j = n; j >= power; j--) {
+            dp[j] = (dp[j] + dp[j - power]) % MOD;
+        }
+    }
+    return dp[n];
+};

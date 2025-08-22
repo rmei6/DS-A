d968ae95b9df33c46bf9ba19eb6ac60d0e05cd84
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2025-08-22 00:00:32 -0400

    math approach

--- JavaScript/Find_the_Minimum_Area_to_Cover_All_Ones_I.js
+++ JavaScript/Find_the_Minimum_Area_to_Cover_All_Ones_I.js
@@ -36,3 +36,24 @@
 // grid[i][j] is either 0 or 1.
 // The input is generated such that there is at least one 1 in grid.
 
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minimumArea = function(grid) {
+    let m = grid.length, n = grid[0].length;
+    let minRow = m, maxRow = -1;
+    let minCol = n, maxCol = -1;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                minRow = Math.min(minRow, i);
+                maxRow = Math.max(maxRow, i);
+                minCol = Math.min(minCol, j);
+                maxCol = Math.max(maxCol, j);
+            }
+        }
+    }
+    return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+};
